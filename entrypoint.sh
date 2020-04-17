89fab5ddc50407d9be0426bcde7b2a6f574f822c
hmharvey <hmharvey@github.com>
2020-04-17 10:18:55 -0700

    test running exec

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 curl -L -O https://github.com/octokit/fixtures-server/releases/download/v6.0.3/fixtures-server-linux
-octokit-fixtures-server
+chmod +x fixtures-server-linux
+./fixtures-server-linux
