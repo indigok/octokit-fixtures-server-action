55a377ebb5fea51772988d0bfe735bc2b53fb46c
hmharvey <hmharvey@github.com>
2020-04-17 11:07:09 -0700

    try v6.0.4

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Hello"
-curl -L -O https://github.com/octokit/fixtures-server/releases/download/v6.0.3/fixtures-server-linux
+curl -L -O https://github.com/octokit/fixtures-server/releases/download/v6.0.4/fixtures-server-linux
 echo "Another hello"
 chmod +x fixtures-server-linux
 echo "A final hello"
