1021a49b74a757d772a972a6572a4e45fbceedb5
hmharvey <hmharvey@github.com>
2020-04-17 10:50:59 -0700

    Add hello output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Hello"
 curl -L -O https://github.com/octokit/fixtures-server/releases/download/v6.0.3/fixtures-server-linux
+echo "Another hello"
 chmod +x fixtures-server-linux
+echo "A final hello"
 ./fixtures-server-linux
