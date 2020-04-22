80ee37b416d8acf841b8b0b64b7d656e99533c61
hmharvey <hmharvey@github.com>
2020-04-21 17:25:36 -0700

    try running the server in the background

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 curl -L -O https://github.com/octokit/fixtures-server/releases/download/v6.0.4/fixtures-server-linux
 chmod +x fixtures-server-linux
 echo "$(ls -l)"
-./fixtures-server-linux
+./fixtures-server-linux &
